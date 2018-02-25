3585cb17ae14fc11a73ce81d73c7c87cc8987bc0
DanielHenry <daniel.henry.39@gmail.com>
2018-02-26 04:17:07 +0700

    Add copy file function call and add return value in addFileRecursively() function

--- main.php
+++ main.php
@@ -3,24 +3,33 @@
 function addFileRecursively($filePath = '', $currentPath = ''){
     $d = dir($currentPath);
     echo $d->path."\n";
+    $success = TRUE;
     while ($entry = $d->read()) {
         if ($entry=='.' || $entry=='..'){
             continue;
         }
         if (is_dir($currentPath.'/'.$entry)){
             $fileType = 'dir';
-            //addFileRecursively($filePath,$currentPath.'/'.$entry);
+            //$success = addFileRecursively($filePath,$currentPath.'/'.$entry);
         } else {
             $fileType = 'not dir';
         }
         echo $entry.' '.$fileType."\n";
-    }        
+    }
+    if ($success){
+        if (copy($filePath,$currentPath)){
+            $success = TRUE;
+        } else {
+            $success = FALSE;
+        }
+    }
+    return $success;
 }
 
 echo $argv[1]."\n";
 echo $argv[2]."\n";
 $filePath = $argv[1];
 $currentPath = $argv[2];
-addFileRecursively($filePath,$currentPath);
+$success = addFileRecursively($filePath,$currentPath);
 
 ?>
