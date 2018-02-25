dc05c2d4f9663753940572d4b7d7831dcc155465
DanielHenry <daniel.henry.39@gmail.com>
2018-02-26 03:43:02 +0700

    Add file iterator in a directory

--- main.php
+++ main.php
@@ -1,6 +1,17 @@
 <?php
 
-echo $argv[1];
-echo $argv[2];
+echo $argv[1]."\n";
+echo $argv[2]."\n";
+$filePath = $argv[1];
+$currentPath = $argv[2];
+
+$d = dir($currentPath);
+echo $d->path."\n";
+while ($entry = $d->read()) {
+    if ($entry=='.' || $entry=='..'){
+        continue;
+    }
+    echo $entry."\n";
+}
 
 ?>
