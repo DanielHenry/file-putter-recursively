cbd93db7117191e3e4dd9e61148e0ce986ed235f
DanielHenry <daniel.henry.39@gmail.com>
2018-02-26 04:01:46 +0700

    Move essential code into addFileRecursively() function

--- main.php
+++ main.php
@@ -1,22 +1,26 @@
 <?php
 
+function addFileRecursively($filePath = '', $currentPath = ''){
+    $d = dir($currentPath);
+    echo $d->path."\n";
+    while ($entry = $d->read()) {
+        if ($entry=='.' || $entry=='..'){
+            continue;
+        }
+        if (is_dir($currentPath.'/'.$entry)){
+            $fileType = 'dir';
+    
+        } else {
+            $fileType = 'not dir';
+        }
+        echo $entry.' '.$fileType."\n";
+    }        
+}
+
 echo $argv[1]."\n";
 echo $argv[2]."\n";
 $filePath = $argv[1];
 $currentPath = $argv[2];
-
-$d = dir($currentPath);
-echo $d->path."\n";
-while ($entry = $d->read()) {
-    if ($entry=='.' || $entry=='..'){
-        continue;
-    }
-    if (is_dir($currentPath.'/'.$entry)){
-        $fileType = 'dir';
-    } else {
-        $fileType = 'not dir';
-    }
-    echo $entry.' '.$fileType."\n";
-}
+addFileRecursively($filePath,$currentPath);
 
 ?>
